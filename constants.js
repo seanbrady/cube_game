b679cbc92a66090464d70964297cccae4319483f
seanbrady <smjbrady@gmail.com>
2016-05-30 17:22:10 -0400

    Add files via upload
    This is what I made before. It's garbage.

---
+++ constants.js
@@ -0,0 +1,23 @@
+//------------
+//System Values
+//------------
+var STAGE_WIDTH = 800,
+	STAGE_HEIGHT = 400,
+	TIME_PER_FRAME = 33, //this equates to 30 fps
+	GAME_FONTS = "bold 20px sans-serif";
+	
+var BOX_WIDTH = 40,
+	BOX_HEIGHT = 40;
+	
+var BOXES_PER_ROW = 3,
+    BOXES_PER_COLUMN = BOXES_PER_ROW;
+
+var ANGLE_THETA = Math.PI/2;
+
+var NUM_OF_COLORED_BOXES =  BOXES_PER_ROW*BOXES_PER_COLUMN - 6;
+	
+// 3D stuff
+
+var SKEW_FACTOR = 0.36;
+	
+	
